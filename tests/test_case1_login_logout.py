3dc383797035e21cc48e862ec0ad9d9ffea60e01
Arkadiy Voronov <voronov_ao@mail.ru>
2023-06-14 14:24:09 +0500

    Facilitated logic for tests

--- tests/test_case1_login_logout.py
+++ tests/test_case1_login_logout.py
@@ -1,24 +1,12 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
-# Create ChromeOptions object and configure headless mode
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-
-# Initialize the ChromeDriver with the options
-driver = webdriver.Chrome(options=chrome_options)
-
-@pytest.fixture
-def browser():
-    # Set up the WebDriver instance
-    driver = webdriver.Chrome()  # You need to have ChromeDriver installed and in PATH
-    yield driver
-    # Teardown - Quit the WebDriver instance
-    driver.quit()
 
 
-def test_login_logout(browser):
-    # Go to the home page
-    browser.get("https://practicetestautomation.com/practice-test-login/")
+@pytest.mark.usefixtures("setup")
+class TestExampleOne:
+    def test_title(self):
+        self.driver.get('https://www.delrayo.tech')
+        assert self.driver.title == "DelRayo.tech - Delrayo Tech"
+    
+    def test_title_blog(self):
+        self.driver.get('https://www.delrayo.tech/blog')
+        print(self.driver.title)

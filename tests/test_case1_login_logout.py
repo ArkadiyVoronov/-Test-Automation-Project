d4815cec2ca4fc32da63908e86cb9686759cbbe0
Arkadiy Voronov <voronov_ao@mail.ru>
2023-06-14 12:55:36 +0500

    add option for headless

--- tests/test_case1_login_logout.py
+++ tests/test_case1_login_logout.py
@@ -1,7 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
+# Create ChromeOptions object and configure headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+# Initialize the ChromeDriver with the options
+driver = webdriver.Chrome(options=chrome_options)
 
 @pytest.fixture
 def browser():
